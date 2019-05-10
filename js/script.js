ffc3ba2b25e56c2535dbacefcbe9171164baba49
Eric Xiao <42785168+mathlord2@users.noreply.github.com>
2019-05-10 19:47:33 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,28 +1,84 @@
-function startTime() {
-	var today = new Date(); //gets the date
-  var a = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]; //gets the month as a word
-  var b = today.getDate(); //gets the day
-  var c = today.getFullYear(); //gets the year
-	var h = today.getHours(); //gets the hour
-	var m = today.getMinutes(); //gets the minute
-	var s = today.getSeconds(); //gets the second
-	var z = "am"
+function time() {
+    var d = new Date();
+    var weekday = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    var month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var date = [4];
 
-	if  (h >= 12) {
-		z = "pm"; //change to pm after noon
-	}
+    var hour = d.getHours();
+    var minute = d.getMinutes();
+    var z = "am"
 
-	if (h > 12) {
-		h -= 12; //time is in pm
-	}
+    if (hour >= 12) {
+        z = "pm";
+    }
 
-	m = checkTime(m);
-	s = checkTime(s);
-	document.getElementById('date').innerHTML = a[today.getMonth()] + " " + b + ", " + c + " " + h + ":" + m + ":" + s + " " + z;
-	var t = setTimeout(startTime, 500);
-	}
+    if (hour > 12) {
+        hour -= 12;
+    }
 
-	function checkTime(i){
-		if (i < 10) {i = "0" + i};
-		return i;
-	}
+    minute = checkTime(minute);
+
+    date[0] = weekday[d.getDay()]
+    date[1] = month[d.getMonth()] + " " + d.getDate();
+    date[2] = d.getFullYear();
+    date[3] = hour + ":" + minute + " " + z;
+
+    return date;
+
+    function checkTime(i) {
+        if (i < 10) { i = "0" + i };
+        return i;
+    }
+}
+
+$(function () {
+    $(".date").html(time().join(", "));
+});
+
+function send() {
+    var input = document.getElementById("input").value;
+    var div = document.createElement("DIV");
+    div.setAttribute("id", "message");
+
+    var text = document.createTextNode("You: " + input);
+    div.appendChild(text);
+
+    var messages = document.getElementById("messages");
+
+    if (input !== "") {
+        messages.insertBefore(div, messages.children[0]);
+    }
+
+    document.getElementById("input").value = "";
+}
+
+function openOverlay() {
+    document.getElementById("overlay").style.display = "block";
+}
+
+function closeOverlay() {
+    document.getElementById("overlay").style.display = "none";
+}
+
+function addContact() {
+    var contacts = document.getElementById("contacts");
+    var div = document.createElement("DIV");
+    div.setAttribute("id", "contact");
+
+    var br = document.createElement("BR");
+    var text1 = document.createTextNode(document.getElementById("Name").value);
+    var text2 = document.createTextNode(document.getElementById("Address").value);
+    
+    if (document.getElementById("Name").value !== "" && document.getElementById("Address").value !== "") {
+        div.appendChild(text1);
+        div.appendChild(br);
+        div.appendChild(text2);
+
+        contacts.appendChild(div);
+
+        document.getElementById("Name").value = "";
+        document.getElementById("Address").value = "";
+    
+        document.getElementById("overlay").style.display = "none";
+    }
+}
